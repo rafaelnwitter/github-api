9b420827b4a94b2ffb04e1595833c92b8d2832ce
Rafael Witt <rafawitt@hotmail.com>
2022-10-31 18:25:56 -0300

    feat: new route

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  assetPrefix: '/docs',
+  rewrites() {
+    return [
+      { source: '/docs/_next/:path*', destination: '/_next/:path*' }
+    ]
+  },
   images: {
     unoptimized: true
   }
